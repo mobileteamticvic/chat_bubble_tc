c9ba3ba83e5107c31e4ad47934eed48d9088922d
Abdul Kadhar <abdulkadhar.sulthanbasha@ticvic.com>
2023-09-07 13:43:49 +0530

    initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:chat_bubble_tc/chat_bubble_widget.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:chat_bubble_tc/chat_bubble_tc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+  final _chatBubbleTcPlugin = ChatBubbleTc();
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // We also handle the message potentially returning null.
+    try {
+      platformVersion = await _chatBubbleTcPlugin.getPlatformVersion() ??
+          'Unknown platform version';
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Column(
+          children: [
+            Center(
+              child: Text('Running on: $_platformVersion\n'),
+            ),
+            // NOTE - Send Bubble widget
+            ChatBubble(
+              message: "Send Example",
+              timeStamp: DateTime.now(),
+              isSendBubble: true,
+            ),
+            // NOTE - Receive Bubble widget
+            ChatBubble(
+              message: "Receive example",
+              timeStamp: DateTime.now(),
+              isSendBubble: false,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
